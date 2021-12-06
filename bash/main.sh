7241f94ef98768618944353557f9788016d0421a
TigranAlexanyan <alex.yan.19@mail.ru>
2021-12-06 21:27:50 +0400

    Creae bash folder

---
+++ bash/main.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+# example script
+
+echo "# test";# echo "# test"
+
+# check the first parameter
+if [ "$1" = "#" ]; then 
+  # test couple of different cases
+  echo "#"; # output \# character 
+  echo '\#'; # output # character '#' for test purpose
+  echo \#\#\#; # comment # comment # comment '# comment'
+  echo \#
+  echo \#;
+  echo \#; # comment
+fi
+# end of the script
+
+
