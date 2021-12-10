15af4c831d91c5e9c4c0fb8b1fcf46c6e15a7765
ssargsyan <s.sargsyan.s@gmail.com>
2021-12-10 09:37:34 +0400

    Update whileSeda.sh

--- Davit/bash_day_4/whileSeda.sh
+++ Davit/bash_day_4/whileSeda.sh
@@ -5,13 +5,11 @@ z=5
 while [ $i -lt $z ]
 do
 j=0
-echo $j
-echo $i
-    while [ $j -le $i ]
-        do
-        echo -n \*
-         j=$(($j + 1))
-       done
- echo " "
- i=$(($i + 1))
+while [ $j -le $i ]
+   do
+     echo -n \*
+     j=$(($j + 1))
+   done
+   echo " "
+   i=$(($i + 1))
 done
